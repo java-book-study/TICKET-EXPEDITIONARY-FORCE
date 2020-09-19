c6131a064dcddeea695d51afa3c74de5ee133f9d
ahnzhx <modunaeggu@naver.com>
2020-09-19 10:36:39 +0900

    first setting

---
+++ src/main/java/com/ticket/captain/CaptainApplication.java
@@ -0,0 +1,13 @@
+package com.ticket.captain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaptainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaptainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ticket/captain/CaptainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticket.captain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaptainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
