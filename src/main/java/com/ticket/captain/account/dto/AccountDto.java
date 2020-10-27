b6b459440b3921b003a2e19eba847e31fa69ccb4
dmstjd1024 <dmstjd1024@naver.com>
2020-10-28 00:44:31 +0900

    [TICKET-58] feat : AccountDto Validation 추가

--- src/main/java/com/ticket/captain/account/dto/AccountDto.java
+++ src/main/java/com/ticket/captain/account/dto/AccountDto.java
@@ -6,6 +6,10 @@ import com.ticket.captain.account.Role;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 
 public class AccountDto {
 
@@ -14,10 +18,16 @@ public class AccountDto {
     @NoArgsConstructor
     public static class Response {
 
+        @NotBlank
         private Long id;
-
+        @NotBlank
+        @Length(min=2, max=20)
         private String name;
+
+        @Email
+        @NotBlank
         private String email;
+        @NotBlank
         private Role role;
 
         public Response(Account account){
@@ -32,8 +42,11 @@ public class AccountDto {
     @NoArgsConstructor
     public static class Update{
 
+        @Length(min=2, max=20)
         private String name;
+        @Email
         private String email;
+
         private Role role;
     }
 
