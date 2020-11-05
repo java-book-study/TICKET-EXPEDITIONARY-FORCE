d1d5992eeed626818e812174ade2dd151120d347
9m1i9n1 <9m1i9n1@gmail.com>
2020-11-06 01:28:14 +0900

    [TICKET-63] fix: 엔티티 수정중..

--- src/main/java/com/ticket/captain/ticket/dto/TicketDto.java
+++ src/main/java/com/ticket/captain/ticket/dto/TicketDto.java
@@ -6,25 +6,39 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class TicketDto {
-    private Long id;
-    private String title;
-    private Integer amount;
+    private Long ticketId;
+    private String ticketNo;
+    private String orderNo;
+    private Long festivalId;
+    private Long festivalSq;
+    private Long statusCode;
+    private BigDecimal price;
 
     @Builder
-    private TicketDto(Long id, String title, Integer amount) {
-        this.id = id;
-        this.title = title;
-        this.amount = amount;
+    private TicketDto(Long ticketId, String ticketNo, String orderNo, Long festivalId, Long festivalSq, Long statusCode, BigDecimal price) {
+        this.ticketId = ticketId;
+        this.ticketNo = ticketNo;
+        this.orderNo = orderNo;
+        this.festivalId = festivalId;
+        this.festivalSq = festivalSq;
+        this.statusCode = statusCode;
+        this.price = price;
     }
 
     public static TicketDto of(Ticket ticket) {
         return TicketDto.builder()
-                .id(ticket.getId())
-                .title(ticket.getTitle())
-                .amount(ticket.getAmount())
+                .ticketId(ticket.getTicketId())
+                .ticketNo(ticket.getTicketNo())
+                .orderNo(ticket.getOrderNo())
+                .festivalId(ticket.getFestivalId())
+                .festivalSq(ticket.getFestivalSq())
+                .statusCode(ticket.getStatusCode())
+                .price(ticket.getPrice())
                 .build();
     }
 }
