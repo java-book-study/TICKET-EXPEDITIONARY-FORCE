70df8dead94b7852f347587197c5e6aaf47997de
9m1i9n1 <9m1i9n1@gmail.com>
2020-11-07 22:58:15 +0900

    [TICKET-63] fix: 티켓 삭제 Api 추가 및 PathParameter 형식의 Api로 변경

--- src/main/java/com/ticket/captain/ticket/TicketController.java
+++ src/main/java/com/ticket/captain/ticket/TicketController.java
@@ -5,26 +5,42 @@ import com.ticket.captain.ticket.dto.TicketCreateDto;
 import com.ticket.captain.ticket.dto.TicketDto;
 import com.ticket.captain.ticket.dto.TicketUpdateDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/ticket")
 public class TicketController {
 
     private final TicketService ticketService;
 
-    @PostMapping("/ticket")
-    public ApiResponseDto<TicketDto> add(@RequestBody TicketCreateDto create) {
+    @GetMapping({"", "/"})
+    public ApiResponseDto<List<TicketDto>> ticketList() {
+        return ApiResponseDto.createOK(ticketService.findAll());
+    }
+
+    @GetMapping("/{ticketId}")
+    public ApiResponseDto<TicketDto> ticketDetail(@PathVariable("ticketId") Long ticketId) {
+        return ApiResponseDto.createOK(ticketService.findById(ticketId));
+    }
+
+    @PostMapping({"", "/"})
+    public ApiResponseDto<TicketDto> ticketAdd(@RequestBody TicketCreateDto create) {
         return ApiResponseDto.createOK(ticketService.add(create.toDto()));
     }
 
-    @PutMapping("/ticket/{id}")
-    public ApiResponseDto<TicketDto> update(Long id, @Valid TicketUpdateDto update) {
-        return ApiResponseDto.createOK(ticketService.update(id, update.toDto()));
+    @PutMapping("/{ticketId}")
+    public ApiResponseDto<TicketDto> ticketUpdate(Long ticketId, @Valid TicketUpdateDto update) {
+        return ApiResponseDto.createOK(ticketService.update(ticketId, update.toDto()));
+    }
+
+    @DeleteMapping("/{ticketId}")
+    public ApiResponseDto<String> delete(@PathVariable("ticketId") Long ticketId) {
+        ticketService.delete(ticketId);
+
+        return ApiResponseDto.DEFAULT_OK;
     }
 }
