b7d442a81999d7921b2b7e7afcf9de5dd5cbc581
9m1i9n1 <9m1i9n1@gmail.com>
2020-11-19 00:15:14 +0900

    [TICKET-82] refactor: Spring Security HttpConfig import 되어있지 않아 추가함

--- src/main/java/com/ticket/captain/config/security/SecurityConfig.java
+++ src/main/java/com/ticket/captain/config/security/SecurityConfig.java
@@ -1,6 +1,9 @@
 package com.ticket.captain.config.security;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
