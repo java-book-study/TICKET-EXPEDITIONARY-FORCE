844200984852f3bec18935011ae8c503fe7d18c3
dmstjd1024 <dmstjd1024@naver.com>
2020-12-04 13:57:46 +0900

    [TICKET-91] refactor : 회원정보를 Account 파라미터로 받아오기 위한 리팩토링

---
+++ src/main/java/com/ticket/captain/account/UserAccount.java
@@ -0,0 +1,18 @@
+package com.ticket.captain.account;
+
+import lombok.Getter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.List;
+
+@Getter
+public class UserAccount extends User {
+
+    public Account account;
+
+    public UserAccount(Account account) {
+        super(account.getEmail(), account.getPassword(), List.of(new SimpleGrantedAuthority("ROLE_USER")));
+        this.account = account;
+    }
+}
