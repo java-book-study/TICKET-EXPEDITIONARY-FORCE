d7396757614ca3a231d52505267ba2a3de99ecb9
dmstjd1024 <dmstjd1024@naver.com>
2021-01-28 23:58:06 +0900

    [TICKET-98] feat : Scrap 문서화

--- src/main/java/com/ticket/captain/scrap/ScrapController.java
+++ src/main/java/com/ticket/captain/scrap/ScrapController.java
@@ -3,11 +3,15 @@ package com.ticket.captain.scrap;
 import com.ticket.captain.account.Account;
 import com.ticket.captain.account.CurrentUser;
 import lombok.RequiredArgsConstructor;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,6 +23,8 @@ public class ScrapController {
     public ResponseEntity<?> createScrap(@CurrentUser Account account, @PathVariable Long festivalId){
 
         Long scrapId = scrapService.createScrap(account, festivalId);
+        EntityModel<Long> scrapModel = EntityModel.of(scrapId).add(linkTo(ScrapController.class).slash(scrapId).withSelfRel());
+        scrapModel.add(Link.of("/docs/index.html#get-scrapId").withRel("profile"));
 
         return ResponseEntity.ok(scrapId);
     }
